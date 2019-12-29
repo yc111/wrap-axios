64ed4bd551b0f498e0375e4d4976fb4292049371
yc111 <yinchuan_111@163.com>
2019-12-29 14:04:04 +0800

    make http request with wrapped api function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,8 +6,18 @@
 
 <script>
 // @ is an alias to /src
+import { fetchData } from '@/api/index';
 
 export default {
   name: 'home',
+  mounted() {
+    fetchData()
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
 };
 </script>
